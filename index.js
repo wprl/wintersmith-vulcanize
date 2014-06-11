f31ba3f06d0cb775f93657acc6803eb77394e04b
William Riley-Land <william@kun.io>
2014-06-11 09:15:20 -0500

    v0.0.1

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 // ## Dependencies
 var vulcanize = require('vulcanize');
 var deco = require('deco');
-var fs = require('fs');
 // ## Module Definition 
 var plugin = module.exports = function (env, callback) {
   var defaults = {};
-  var options = deco.merge(defaults, env.config.vulcanize);  
-  var VulcanizePlugin = deco().inherit(ContentPlugin);
+  var options = deco.merge(defaults, env.config.vulcanize);
+  var VulcanizePlugin = deco(function (filepath) {
+    var fileDefaults = {
+      input: filepath,
+      outputDir: '/'
+    };
+    var fileOptions = deco.merge(fileDefaults, options)
+    this.vulcanized = vulcanize.processDocument(fileOptions);
+  });
+
+  VulcanizePlugin.inherit(env.ContentPlugin);
+
+  VulcanizePlugin.prototype.getFilename = function() {
+    return this.filepath.relative;
+  };
 
   VulcanizePlugin.prototype.getView = function() {
     return function (env, locals, contents, templates, callback) {
-      vulcanize.setOptions(options);
-      callback(null, vulcanize.processDocument());
+      callback(null, this.vulcanized);
     };
   };
 
+  VulcanizePlugin.prototype.fromFile = function (filepath, callback) {
+    return callback(null, new VulcanizePlugin(filepath));
+  };
+
+  env.registerContentPlugin('vulcanize', '**/*.html', VulcanizePlugin);
+
   return VulcanizePlugin;
 };
-
-env.registerContentPlugin('scripts', '**/*.html', VulcanizePlugin);
